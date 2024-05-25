2bb4e6a8c70d24a9b4bb5a31eb622ec1a73b4c44
Juan Pablo <juanpasan2000@gmail.com>
2024-05-24 21:05:48 -0500

    Get methods for User Resource

---
+++ src/main/java/com/spring/rest/webservices/restful_web_services/user/UserController.java
@@ -0,0 +1,28 @@
+package com.spring.rest.webservices.restful_web_services.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService service;
+
+    public UserController(UserDaoService userDaoService) {
+        this.service = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findUser(id);
+    }
+
+}
--- src/main/java/com/spring/rest/webservices/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/spring/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -17,8 +18,22 @@ public class UserDaoService {
         users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
     }
 
+    /***
+     * Finds all the users
+     * @return All users
+     */
     public List<User> findAll() {
         return users;
+    }
+
+    /***
+     * Finds a user with a specific id
+     * @param id
+     * @return User
+     */
+    public User findUser(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 
 }
