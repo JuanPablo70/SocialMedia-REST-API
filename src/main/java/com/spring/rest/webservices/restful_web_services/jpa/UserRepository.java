1ee3a88a8fad7aef77a6be4bdff3b3fc6d57a3a9
Juan Pablo <juanpasan2000@gmail.com>
2024-05-29 20:49:23 -0500

    REST API connected to H2 using JPA and Hibernate

---
+++ src/main/java/com/spring/rest/webservices/restful_web_services/jpa/UserRepository.java
@@ -0,0 +1,7 @@
+package com.spring.rest.webservices.restful_web_services.jpa;
+
+import com.spring.rest.webservices.restful_web_services.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
--- src/main/java/com/spring/rest/webservices/restful_web_services/user/UserController.java
+++ src/main/java/com/spring/rest/webservices/restful_web_services/user/UserController.java
@@ -2,6 +2,7 @@ package com.spring.rest.webservices.restful_web_services.user;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.spring.rest.webservices.restful_web_services.jpa.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -11,29 +12,30 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
-    private UserDaoService service;
+    private UserRepository repository;
 
-    public UserController(UserDaoService userDaoService) {
-        this.service = userDaoService;
+    public UserController(UserRepository repository) {
+        this.repository = repository;
     }
 
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
-        return service.findAll();
+        return repository.findAll();
     }
 
     @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
-        User user = service.findUser(id);
-        if (user == null) {
+        Optional<User> user = repository.findById(id);
+        if (user.isEmpty()) {
             throw new UserNotFoundException("id: " + id);
         }
 
-        EntityModel<User> entityModel = EntityModel.of(user);
+        EntityModel<User> entityModel = EntityModel.of(user.get());
 
         // Link pointing to the retrieveUsers method
         WebMvcLinkBuilder link = linkTo((methodOn(this.getClass())).retrieveAllUsers());
@@ -44,7 +46,7 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = service.save(user);
+        User savedUser = repository.save(user);
         
         // Indicates the location of the created user to the header
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -57,7 +59,7 @@ public class UserController {
 
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        service.deleteUser(id);
+        repository.deleteById(id);
     }
 
 }
